0afdc220df32a66a774dc6e99af9fba677fa9c0f
Rebecca Manzi <rebeccamanzi@gmail.com>
2020-01-22 10:12:53 -0300

    Versão 1

---
+++ Teste/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Teste
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("Bom dia!");
+            Console.WriteLine("Como você está?");
+            Console.ReadLine();
+
+
+        }
+    }
+}
